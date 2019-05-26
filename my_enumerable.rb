80edab849bea65c75a48a84d16936374e3e5f0c0
Xarri Jorge <kodinglife@gmail.com>
2019-05-26 13:20:53 +0000

    Added more methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -11,3 +11,35 @@ module Enumerable
         end
     end
 
+    def my_each_with_index
+        if block_given?
+            for i in 0..self.length do
+                yield(self[i], i)
+            end 
+        else
+            self
+        end
+    end
+
+    def my_map
+        arr = []
+        if block_given?
+            self.my_each {|x| arr << yield(x)}
+        else
+            arr
+        end
+        arr
+    end
+
+    def my_count
+        if block_given?
+            count = 0
+            self.my_each do |x|
+                count += 1 if yield(x)
+            end
+        else
+            count = self.length
+        end
+        count
+    end
+
